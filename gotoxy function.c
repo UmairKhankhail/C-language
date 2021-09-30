44b4ba03ceb719aa13deb839c7a94048dd3beb65
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-09-30 12:06:20 +0500

    prinf func which prints variables

---
+++ gotoxy function.c
@@ -0,0 +1,18 @@
+
+main()
+{
+    int a=100;
+    float b=55.22;
+    printf(" a=%d ,b=%f\n",a,b);
+    printf("Sum of values %d and %f is %f",a,b,a+b);
+}
+
+
+/*
+Format Specifiers
+
+%d int
+%f float
+%c char
+%lf double
+*/
