d0edee3e2d4292c931f365b9b4c7574108e61a3c
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-09-30 13:07:42 +0500

    a function which is used inplace of gotoxy in code:blocks

---
+++ SetConsoleCursorPosition.c
@@ -0,0 +1,15 @@
+#include<windows.h>
+void gotoxy(int x, int y)
+{
+    COORD c;
+    c.X=x;
+    c.Y=y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);
+}
+
+main()
+{
+    gotoxy(55,2);
+    printf("hello!");
+    getch();
+}
