a7feb22f007eec43b9ccd80a0015a8d65f91e4dd
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-09-30 16:29:38 +0500

    Uses of modulus operator.

---
+++ modulus % operator.c
@@ -0,0 +1,13 @@
+// used to check divisibility
+// used to find the last,second last digit when divided by 10, 100 respectively
+
+//  the sign of numerator(digit on the left side of modulus) is always the sign of remainder
+
+main()
+{
+    int a=345%55;
+    printf("%d",a);
+    getch();
+
+}
+
