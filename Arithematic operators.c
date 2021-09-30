b7ef793d72c42ee795723834edfe2f700db7d6b1
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-09-30 16:38:46 +0500

    to check to precedence of different arithematic operators

---
+++ Arithematic operators.c
@@ -0,0 +1,16 @@
+// Arithematics operator are solved from Left to Right order
+
+main()
+{
+    int x;
+    x=100/50-1+3;
+    printf("%d",x);
+    getch();
+}
+
+
+/*
+3/4    0
+3.0/4  0.75
+3/4.0  0.75
+*/
