82e6cd6f6b11b680b735e6d30b182814325df1ef
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-10-01 12:13:18 +0500

    expresssion 1 ? expression 2 : expression 3

---
+++ conditional operators.c
@@ -0,0 +1,11 @@
+// also called ternary operator
+// expression1 ? expression2 : expression3
+
+main()
+{
+    int x,y;
+    x=5>4?1,2,3,4,5:1,2,3,4,5;
+    y=5<4?1,2,3,4,5:1,2,3,4,5;
+    printf("%d\n",x);
+    printf("%d",y);
+}
