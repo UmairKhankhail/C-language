7ca583b9bc0267670ac6dfcbbbd8bb37a980ed23
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-10-21 22:04:06 +0500

    done

--- multiple strings.c
+++ multiple strings.c
@@ -1,1 +1,17 @@
-main
+main()
+{
+    char a[3][10]={};
+    int i;
+    for (i=0;i<=2;i++)
+       {
+         gets(&a[i][0]);
+
+       }
+    for (i=0;i<=2;i++)
+        {
+        printf("%s \n" ,a[i]);
+
+        }
+
+        getch();
+}
