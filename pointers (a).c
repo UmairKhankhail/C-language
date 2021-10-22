38765e941757ae1ad5be922c4f5f21d4fa42b57d
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-10-22 10:57:36 +0500

    introduction to pointers

---
+++ pointers (a).c
@@ -0,0 +1,8 @@
+main()
+{
+    int a=5;
+    printf("%u \n",a);
+    printf("%u \n",&a);  // & refrencing opertator
+    printf("%d ",*&a); //* refrencing , takes address as an operand,returns variable name
+
+}
