829e06ada36d49d19b810375fe8259da66876a95
UmairKhankhail <59060383+UmairKhankhail@users.noreply.github.com>
2021-10-29 11:11:01 +0500

    modified

--- malloc functions.c
+++ malloc functions.c
@@ -1,9 +1,12 @@
+
+// This function allocates memory to a single memory block
+
 main()
 {
 
     float *p;  // static memory allocation
     p=(float*)malloc(4);  // dynamic memory allocation
     *p=3.4;
-    printf("%f",*&p);
+    printf("%f",p);
     getch();
 }
